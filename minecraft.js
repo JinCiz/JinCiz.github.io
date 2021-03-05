50461a45322d7343169fdc1a8ab53e74b1110165
JinCiz <80063748+JinCiz@users.noreply.github.com>
2021-03-05 11:45:08 +0800

    Minecraft
    s

---
+++ minecraft.js
@@ -0,0 +1,9 @@
+function pop(){alert("i'm just kidding ")}
+
+/*var x = prompt('How old are you')
+var d = Number(x)
+f(d<18){window.alert('you are too young')}
+else if (18<=d && d<=30){window.alert("you are in the good age")}
+else if (d>30){window.alert("i don't care")}
+else {window.alert("This is not a valid number.")}*/
+function abbr(){document.getElementById('20').innerHTML = Math.round(Math.random()*9)}
